7d1abc2ed752c87f4d75a5dd394a47a80f770384
Tasnuva Islam <tasnuvamarziya60@gmail.com>
2023-10-17 23:15:15 +0600

    Footer added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,3 +1,4 @@
+import Footer from "../Components/Footer";
 import Header from "../Components/Header";
 import Navbar from "../Components/Navbar";
 
@@ -6,6 +7,7 @@ const Home = () => {
         <div>
             <Navbar></Navbar>
             <Header></Header>
+            <Footer></Footer>
         </div>
     );
 };
