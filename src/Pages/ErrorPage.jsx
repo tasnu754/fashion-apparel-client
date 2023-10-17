1a120b6f82b86cbf602e855af06b021bb9838e3b
Tasnuva Islam <tasnuvamarziya60@gmail.com>
2023-10-17 20:01:31 +0600

    Error page addeed

---
+++ src/Pages/ErrorPage.jsx
@@ -0,0 +1,12 @@
+
+const ErrorPage = () => {
+    return (
+        <div className="text-center  mt-80 font-bold">
+            <h1 className="text-5xl">404 ERROR!!!</h1>
+            <h3 className="text-3xl">There is Something Wrong!</h3>
+            <p className="text-2xl">We can not seem to find the page you looking for.</p>
+        </div>
+    );
+};
+
+export default ErrorPage;
---
+++ src/Pages/MainPage.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+
+const MainPage = () => {
+    return (
+        <div>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default MainPage;
